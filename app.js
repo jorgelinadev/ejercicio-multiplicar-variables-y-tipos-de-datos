0da83a0ab5c58dd212c6b9ce52d1af159fd1a6a1
jorgelinadev <jorgelinalberti@gmail.com>
2022-07-08 10:11:24 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,9 @@
+//alert ("Mi archivo está bien linkeado")
+
+const multiplicar = (a,b) => {
+    const valor1 = a ;
+    const valor2 = b ; 
+    return a * b ;
+}
+
+console.log (multiplicar (5,3));
